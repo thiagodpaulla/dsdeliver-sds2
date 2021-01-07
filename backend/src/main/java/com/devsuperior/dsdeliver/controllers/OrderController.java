ec17b53757490d994f18fde5d37037e7cb13efb6
thiago vieira <sesthi@gmail.com>
2021-01-07 20:27:44 -0300

    Ready to prod

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/OrderController.java
@@ -1,63 +1,53 @@
-package com.devsuperior.dsdeliver.services;
+package com.devsuperior.dsdeliver.controllers;
 
-import java.time.Instant;
+import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
-import com.devsuperior.dsdeliver.dto.ProductDTO;
-import com.devsuperior.dsdeliver.entities.Order;
-import com.devsuperior.dsdeliver.entities.OrderStatus;
-import com.devsuperior.dsdeliver.entities.Product;
-import com.devsuperior.dsdeliver.repositories.OrderRepository;
-import com.devsuperior.dsdeliver.repositories.ProductRepository;
+import com.devsuperior.dsdeliver.services.OrderService;
 
-@Service
-public class OrderService {
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderController {
 	
+	//Dependency injection:
 	@Autowired
-	private OrderRepository repository;
+	private OrderService service;
 	
-	@Autowired
-	private ProductRepository productRepository;
 	
-	@Transactional(readOnly = true)
-	public List<OrderDTO> findAll() {
-		List<Order> list = repository.findOrdersWithProducts();
-		return list.stream().map(element -> new OrderDTO(element)).collect(Collectors.toList());
+	@GetMapping
+	public ResponseEntity<List<OrderDTO>> findAll() {
+		List<OrderDTO> list = service.findAll();
+		//Return a object of type ResponseEntity:
+		return ResponseEntity.ok().body(list);
 	}
 	
-	@Transactional
-	public OrderDTO insert(OrderDTO dto) {
-		//Create a new instance of the Order entity with the attributes:
-		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
-								Instant.now(), OrderStatus.PENDING);
-		//For each ProductDTO in the OrderDTO's product list:
-		for (ProductDTO p : dto.getProducts()) {
-			//Create a new instance of the Product entity and associate it with the respective Order:
-			Product product = productRepository.getOne(p.getId());
-			order.getProducts().add(product);
-		}
-		//Save the order in the database:
-		order = repository.save(order);
-		//Return the OrderDTO object:
-		return new OrderDTO(order);
+
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+		
 	}
 	
-	@Transactional
-	public OrderDTO setDelivered(Long id) {
-		//Get the specified Order by id:
-		Order order = repository.getOne(id);
-		//Set the new status:
-		order.setStatus(OrderStatus.DELIVERED);
-		//Save the order in the database:
-		order = repository.save(order);
-		//Return the OrderDTO object:
-		return new OrderDTO(order);
-	}
+
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+		OrderDTO dto = service.setDelivered(id);
+		return ResponseEntity.ok().body(dto);
+	} 	 
 	
+
 }
--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,9 +22,42 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
-		List<Order> list = repository. findAll();
-		return list.stream().map(element -> new OrderDTO()).collect(Collectors.toList());
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(element -> new OrderDTO(element)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		//Create a new instance of the Order entity with the attributes:
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(),
+								Instant.now(), OrderStatus.PENDING);
+		//For each ProductDTO in the OrderDTO's product list:
+		for (ProductDTO p : dto.getProducts()) {
+			//Create a new instance of the Product entity and associate it with the respective Order:
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		//Save the order in the database:
+		order = repository.save(order);
+		//Return the OrderDTO object:
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		//Get the specified Order by id:
+		Order order = repository.getOne(id);
+		//Set the new status:
+		order.setStatus(OrderStatus.DELIVERED);
+		//Save the order in the database:
+		order = repository.save(order);
+		//Return the OrderDTO object:
+		return new OrderDTO(order);
+	}
+	
 }
