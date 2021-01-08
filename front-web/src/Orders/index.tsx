613a31cbc7cbf4edf0a931242164e01e2d282110
thiago vieira <sesthi@gmail.com>
2021-01-08 11:40:40 -0300

    order

---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import './styles.css';
+import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from '../api';
+
+function Orders() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetchProducts()
+      .then(response => setProducts(response.data))
+      .catch(error => console.log(error))
+  }, []);
+
+  return (
+      <div className="orders-container">
+          <StepsHeader />
+          <ProductsList products={products} />
+      </div>
+  )
+}
+
+export default Orders;
+© 2021 GitHub, Inc.
