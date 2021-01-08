623d26a6095e22b3c80dae32a3012fa6e8476027
thiago vieira <sesthi@gmail.com>
2021-01-08 11:29:49 -0300

    footer

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-hellow sds2        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Navbar />
+			<Home />
+		</>
+	);
 }
 
 export default App;
